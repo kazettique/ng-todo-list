d808dd3140bfae83cf3aaa5809ff7290bee152db
Woody <woody.chang@cloud-interactive.com>
2021-03-15 18:17:12 +0800

    Start todo list tutorial

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+
+// Service
+import { TodoListService } from './todo-list.service';
+
+// Class
+import { Todo } from './todo.model';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+
+  constructor(private todoListService: TodoListService) { }
+
+  ngOnInit(): void {
+  }
+
+  addTodo(inputRef: HTMLInputElement): void {
+    const todo = inputRef.value.trim();
+
+    if (todo) {
+      this.todoListService.add(todo);
+      inputRef.value = '';
+    }
+  }
+
+  getList(): Todo[] {
+    return this.todoListService.getList();
+  }
+
+  remove(index: number): void {
+    this.todoListService.remove(index);
+  }
+
+  edit(todo: Todo): void {
+    todo.editable = true;
+  }
+
+  update(todo: Todo, newTitle: string): void {
+    if (!todo.editing) {
+      return;
+    }
+
+    const title = newTitle.trim();
+
+    if (title) {
+      todo.setTitle(title);
+      todo.editable = false;
+    } else {
+      const index = this.getList().indexOf(todo);
+      if (index !== -1) {
+        this.remove(index);
+      }
+    }
+  }
+
+  cancelEditing(todo: Todo): void {
+    todo.editable = false;
+  }
+
+}
---
+++ src/app/todo-list/todo-list.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+// Class
+import { Todo } from './todo.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoListService {
+  private list: Todo[] = [];
+
+  constructor() { }
+
+  getList(): Todo[] {
+    return this.list;
+  }
+
+  add(title: string): void {
+    // 避免傳入的 title 是無效值或空白字串，稍微判斷一下
+    if (title || title.trim()) {
+      this.list.push(new Todo(title));
+    }
+  }
+
+  remove(index: number): void {
+    this.list.splice(index, 1);
+  }
+
+}
